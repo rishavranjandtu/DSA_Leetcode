7c6bf43ec2d990d4b797bf686aae3c70214b4c26
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2022-06-22 20:22:10 +0530

    Time: 11 ms (23.04%), Space: 11.9 MB (93.09%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -4,32 +4,28 @@ public:
         int breakpoint=-1;
         int next_no;
         int n=nums.size();
-        for(int i=n-1;i>=1;i--)
-        {
-            if(nums[i]>nums[i-1])
-            {
-                breakpoint=i-1;
-                break;
-            }
-        }
         
-        if(breakpoint!=-1)
-        {
-            for(int j=n-1;j>=0;j--)
-            {
-                if(nums[j]>nums[breakpoint])
-                {
+        for(int i=n-1;i>=1;i--){
+            if(nums[i]>nums[i-1]){
+                 breakpoint=i-1;
+                 break;}}
+        
+        if(breakpoint!=-1){
+            for(int j=n-1;j>=0;j--) {
+                if(nums[j]>nums[breakpoint]){
                     next_no=j;
-                    break;
-                }
-            }
-           // cout<<breakpoint<<" "<<next_no;
+                    break;}}
+           
             swap(nums[breakpoint],nums[next_no]);
-            reverse(nums.begin()+breakpoint+1,nums.end());
-        }
-        else
-        {
-            reverse(nums.begin(),nums.end());
-        }
+            reverse(nums.begin()+breakpoint+1,nums.end());}
+        
+        else{
+            reverse(nums.begin(),nums.end());}
+        
+        ////////////////////////////
+        //inbuilt function
+        /*
+        next_permutation(nums.begin(),nums.end());
+        */
     }
 };
