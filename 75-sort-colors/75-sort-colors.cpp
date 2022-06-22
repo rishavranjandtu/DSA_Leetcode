f115cb1abfec0f299e6eac1789605aa42fcaa33b
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2022-06-22 12:31:26 +0530

    Time: 3 ms (61.31%), Space: 8.2 MB (90.53%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+   //////////////////////////////////////////////////
+    //O(N^2)
     void sortColors(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++)
+       /* for(int i=0;i<nums.size();i++)
         {
             for(int j=0;j<nums.size()-1-i;j++)
             {
@@ -10,6 +12,24 @@ public:
                     swap(nums[j],nums[j+1]);
                 }
             }
+        }*/
+        /////////////////////////////////////////////
+        //O(2N)
+        vector<int> v(3,0);
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0) v[0]++;
+            else if(nums[i]==1) v[1]++;
+            else v[2]++;
         }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i<v[0]) {nums[i]=0;}
+            else if(v[0]<=i&&i<v[1]+v[0]){nums[i]=1;}
+            else{ nums[i]=2;}
+        }
+        
     }
 };
