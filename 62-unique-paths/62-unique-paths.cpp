6e8983dd54a1d1ca337b978cc1a557585fb6fa68
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2022-06-28 14:03:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.88%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    
-    int count_p(int i,int j,int m,int n,vector<vector<int>>&dp)
+    ///////////Dp approach//////////////////
+   /* int count_p(int i,int j,int m,int n,vector<vector<int>>&dp)
     {
         if(i>=m||j>=n) return 0;
         
@@ -11,8 +11,24 @@ public:
        if(dp[i][j]!=-1) return dp[i][j];
         else return dp[i][j]= count_p(i+1,j,m,n,dp)+count_p(i,j+1,m,n,dp);
     }
+    
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
         return count_p(0,0,m,n,dp);
+    
+    */
+    
+    //////////////best approach///////////
+    int uniquePaths(int m, int n) {
+    int N = n+m-2;
+        int r = m-1;
+        double res = 1;
+        
+        for(int i=1; i<=r; i++){
+            res = res * ( N - r + i) / i;
+        }
+        
+        return (int)res;
     }
+    ///////////////////////////////////////
 };
