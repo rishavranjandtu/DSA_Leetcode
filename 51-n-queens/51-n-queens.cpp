81af31fa054480e9e7ed1c9bc9aa7f0e1444bd1a
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2022-08-24 23:15:11 +0530

    Time: 13 ms (47.31%), Space: 7.2 MB (85.20%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,74 +1,56 @@
 class Solution {
 public:
-     vector<vector<string>>d;
-    bool place(vector<string>&ans, int n, int i, int j)
+    bool check(int i, int j,vector<vector<string>>&ans, vector<string>&a,int n)
     {
-        //col
-        for(int k=0;k<=i;k++)
+        for(int k=0;k<=j;k++)
         {
-            if(ans[k][j]=='Q')
-            {
-                return false;
-            }
+            if(a[i][k]=='Q') return false;
         }
-        //ld
-
+         for(int k=0;k<=i;k++)
+        {
+            if(a[k][j]=='Q') return false;
+        }
         int x=i;
         int y=j;
         while(x>=0&&y>=0)
         {
-            if(ans[x][y]=='Q')
-            {
-                return false;
-            }
-            x--;
-            y--;
+            if(a[x--][y--]=='Q') return false;
         }
         x=i;
         y=j;
-         while(x>=0&&y<n)
-        {
-            if(ans[x][y]=='Q')
-            {
-                return false;
-            }
-            x--;
-            y++;
+        while(x>=0&&y<=n-1)
+{
+            if(a[x--][y++]=='Q') return false;
         }
-        
         return true;
         
     }
-    
-    
-    bool nq(vector<string>&ans, int n, int i)
+    bool nqueen(int i, int n, vector<string>&a, vector<vector<string>>&ans)
     {
-        
-        if(i==n)
+        if(i==n)        
         {
-            d.push_back(ans);
+            ans.push_back(a);
             return false;
         }
         
         for(int j=0;j<n;j++)
         {
-            if(place(ans,n,i,j))
+            if(check(i,j,ans,a,n))
             {
-                ans[i][j]='Q';
-                bool next=nq(ans,n,i+1);
-                if(next)
+                a[i][j]='Q';
+                if(nqueen(i+1,n,a,ans))
                 {
                     return true;
                 }
-                ans[i][j]='.';
+                a[i][j]='.';
             }
         }
         return false;
     }
-    
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> ans(n, string(n,'.'));
-        nq(ans,n,0);
-        return d;
+        vector<vector<string>>ans;
+        vector<string> a(n,string(n,'.'));
+        nqueen(0,n,a,ans);
+        return ans;
     }
 };
