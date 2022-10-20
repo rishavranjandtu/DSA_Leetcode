290419ab1e198f6662ebb6ba468f7c4ebdfc646a
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2022-10-20 10:06:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.83%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countTime(string time) {
+         int ans=1;
+        if(time[4]=='?') ans=ans*10;
+        if(time[3]=='?') ans=ans*6;
+
+        if(time[1]=='?'&&time[0]=='?') ans=ans*24;
+        else
+        {
+            if(time[0]=='?')
+            {
+                if(time[1]<'4') ans=ans*3;
+                else ans=ans*2;
+
+            }
+
+            if(time[1]=='?')
+            {
+                if(time[0]=='2') ans=ans*4;
+                else ans=ans*10;
+            }
+        }        
+        return ans;
+        
+    }
+};
