dcb1a561c81a87dbb5efbac1a4359f481d158638
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2022-12-19 18:48:15 +0530

    Time: 1318 ms (47.12%), Space: 213.8 MB (24.61%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& e, int source, int d) {
-        //bfs
-      unordered_map<int,vector<int>>m;
-      queue<int>q;
+  bool dfs(vector<vector<int>>& e, int s, int d,  vector<int>&vis)
+  {
+    if(s==d) return true;
+    if(vis[s]==true) return false;
+    vis[s]=1;
+    for(auto x:e[s])
+    {
+      if(!vis[x])
+      {
+         if(dfs(e,x,d,vis)) return true;
+      }
+    }
+    return false;
+  }
+    bool validPath(int n, vector<vector<int>>& e, int s, int d) {
+        vector<vector<int>>v(n);
       for(int i=0;i<e.size();i++)
       {
-        m[e[i][0]].push_back(e[i][1]);
-        m[e[i][1]].push_back(e[i][0]);
+       
+          v[e[i][0]].push_back(e[i][1]);
+          v[e[i][1]].push_back(e[i][0]);
+        
       }
       
       vector<int>vis(n,0);
-      q.push(source);
-      vis[source]=1;
-      while(!q.empty())
-      {
-        int f=q.front();
-        q.pop();
-        if(f==d) return true;
-        for(auto x:m[f])
-        {
-          if(!vis[x])
-             {
-               q.push(x);
-            vis[x]=1;
-             }
-        }
-      }
-      return false;
+       return dfs(v,s,d,vis);
     }
 };
