c877839c62d9263ab4034e829247e076d6e55a4e
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-01-02 21:19:58 +0530

    Time: 86 ms (72.62%), Space: 21.8 MB (46.60%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
+         int n=grid.size();
       int m=grid[0].size();
-      queue<pair<int,int>>q;
-      q.push({0,0});
+      vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
+      dis[0][0]=0;
+      queue<pair<int,pair<int,int>>>q;
+      q.push({1,{0,0}});
       if(grid[0][0]==1) return -1;
-      grid[0][0]=1;
+      if(n==1&&m==1&&grid[0][0]==0) return 1;
       while(!q.empty())
       {
-        
-        int r=q.front().first;
-        int c=q.front().second;
-        if(r==n-1&&c==m-1) return grid[r][c];
+        int d=q.front().first;
+        int r=q.front().second.first;
+        int c=q.front().second.second;
         q.pop();
         for(int i=-1;i<=1;i++)
         {
@@ -23,10 +24,11 @@ public:
             {
               int nr=r+i;
               int nc=c+j;
-              if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==0)
+              if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==0&&1+dis[r][c]<dis[nr][nc])
               {
-                q.push({nr,nc});
-                grid[nr][nc]=1+grid[r][c];
+                if(nr==n-1&&nc==m-1) return d+1;
+                dis[nr][nc]=1+dis[r][c];
+                q.push({d+1,{nr,nc}});
               }
             }
           }
