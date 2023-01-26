19fcb775d0e3c07a2b6d648a624ba0ed9ee72845
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-01-26 23:53:32 +0530

    Time: 26 ms (76.03%), Space: 13 MB (88.56%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>>adj[n];
+      vector<pair<int,int>>adj[n];
       for(int i=0;i<flights.size();i++)
       {
         adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
