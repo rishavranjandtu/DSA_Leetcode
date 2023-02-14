64f6cc8261d768b14d00a143f564d3446609d88f
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-02-15 01:26:38 +0530

    Time: 2 ms (73.98%), Space: 6.3 MB (86.66%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+         int carry=0;
+         string ans="";
+        while(a.empty()==false||b.empty()==false||carry==1)
+        {
+            int sum=carry;
+            if(a.empty()==false)
+            {
+                sum+=a.back()-'0';
+                a.pop_back();
+            }
+            if(b.empty()==false)
+            {
+                sum+=b.back()-'0';
+                b.pop_back();
+            }
+            ans+=sum%2+'0';
+            carry=sum/2;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
