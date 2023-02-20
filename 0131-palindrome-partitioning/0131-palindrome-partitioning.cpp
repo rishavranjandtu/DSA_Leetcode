19945fe28acdddb1f38692a02b42b6427f25d258
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-02-20 14:30:33 +0530

    Time: 101 ms (90.87%), Space: 49.1 MB (99.19%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-  bool ispal(string s)
+  bool ispalindrome(string s)
   {
     int l=0;
     int r=s.size()-1;
     while(l<r)
     {
-      if(s[l]!=s[r]) return false;
-      l++;
-      r--;
+      if(s[l++]!=s[r--]) return false;
     }
     return true;
   }
-    vector<vector<string>>ans;
-  void rec(string s, vector<string>&v)
+  void fun(string s, vector<string>&v, vector<vector<string>>&ans)
   {
     if(s.size()==0)
     {
       ans.push_back(v);
       return;
     }
-   
-    
+    string a="";
     for(int i=0;i<s.size();i++)
     {
-      string pre=s.substr(0,i+1);
-      string left=s.substr(i+1);
-      if(ispal(pre))
+      a=a+s[i];
+      string l=s.substr(i+1);
+      if(ispalindrome(a))
       {
-        v.push_back(pre);
-        rec(left,v);
+        v.push_back(a);
+        fun(l,v,ans);
         v.pop_back();
       }
     }
   }
     vector<vector<string>> partition(string s) {
-      vector<string>v;
-      rec(s,v);
+        vector<string>v;
+      vector<vector<string>>ans;
+      fun(s,v,ans);
       return ans;
-        
     }
 };
