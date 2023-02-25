8e14fbd1134dc627a356087db1107de9736f1bd3
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-02-25 18:12:44 +0530

    Time: 9 ms (42.63%), Space: 8 MB (53.72%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+      
         vector<int>dp(n+1,0);
-      for(int i=0;i<=n;i++)
+      dp[0]=0;
+      for(int i=1;i<=n;i++)
       {
-        int j=i;
-        int c=0;
-        while(j)
-        {
-          if(j&1==1) c++;
-          j=j>>1;
-        }
-        dp[i]=c;
+        dp[i]=dp[i/2]+(i%2!=0);
       }
       return dp;
     }
