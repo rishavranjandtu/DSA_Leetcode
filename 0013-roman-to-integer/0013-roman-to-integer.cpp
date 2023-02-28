d0d6c9471bac836fdd9b6b8374cf4eebb9806d94
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-02-28 15:25:16 +0530

    Time: 21 ms (32.41%), Space: 8.2 MB (36.60%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -9,35 +9,14 @@ public:
       m['C']=100;
       m['D']=500;
       m['M']=1000;
-      int ans=0;
-      int i=0;
+     int ans=0;
       int n=s.size();
-      while(i<n)
+      for(int i=1;i<n;i++)
       {
-        if(s[i]=='I')
-        {
-          if(s[i+1]=='V') {ans+=4; i=i+2;}
-          else if(s[i+1]=='X') {ans+=9; i=i+2;}
-          else {ans+=1;i++;}
-        }
-        else if(s[i]=='X')
-        {
-          if(s[i+1]=='L') {ans+=40; i=i+2;}
-          else if(s[i+1]=='C') {ans+=90; i=i+2;}
-          else {ans+=10;i++;}
-        }
-        else if(s[i]=='C')
-        {
-          if(s[i+1]=='D') {ans+=400; i=i+2;}
-          else if(s[i+1]=='M') {ans+=900; i=i+2;}
-          else {ans+=100; i++;}
-        }
-        else
-        {
-          ans+=m[s[i]];
-          i++;
-        }
+        if(m[s[i-1]]<m[s[i]]) ans-=m[s[i-1]];
+        else ans+=m[s[i-1]];
       }
+      ans+=m[s[n-1]];
       return ans;
     }
 };
