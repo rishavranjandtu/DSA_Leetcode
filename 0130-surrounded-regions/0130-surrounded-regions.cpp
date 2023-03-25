cda562a839af716adc03f91449742c78daff7979
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-03-25 21:21:34 +0530

    Time: 11 ms (88.34%), Space: 10.5 MB (34.11%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,39 +2,40 @@ class Solution {
 public:
   int n;
   int m;
-  int dr[4]={0,0,1,-1};
-  int dc[4]={-1,1,0,0};
-  void dfs(int a, int b, vector<vector<int>>&vis,vector<vector<char>>&board)
-  {
-    vis[a][b]=1;
-    for(int i=0;i<4;i++)
+  int a[4]={0,0,-1,1};
+  int b[4]={-1,1,0,0};
+   void dfs(int x, int y, vector<vector<int>>&vis, vector<vector<char>>&board)
+   {
+     vis[x][y]=1;
+  for(int i=0;i<4;i++)
+   {
+    int nr=x+a[i];
+    int nc=y+b[i];
+    if(nr>=0&&nr<n&&nc>=0&&nc<m&&!vis[nr][nc]&&board[nr][nc]=='O')
     {
-      int x=a+dr[i];
-      int y=b+dc[i];
-      if(x>=0&&x<n&&y>=0&&y<m&&board[x][y]=='O'&&!vis[x][y])
-      {
-        dfs(x,y,vis,board);
-      }
+        dfs(nr,nc,vis,board);
     }
-    
-  }
+   }
+   }
     void solve(vector<vector<char>>& board) {
        n=board.size();
-      m=board[0].size();
+       m=board[0].size();
       vector<vector<int>>vis(n,vector<int>(m,0));
+      
       for(int i=0;i<n;i++)
       {
         for(int j=0;j<m;j++)
         {
           if(i==0||i==n-1||j==0||j==m-1)
           {
-            if(board[i][j]=='O')
+            if(board[i][j]=='O'&&!vis[i][j])
             {
-             dfs(i,j,vis,board); 
+            dfs(i,j,vis,board);
             }
           }
         }
       }
+      
       for(int i=0;i<n;i++)
       {
         for(int j=0;j<m;j++)
@@ -45,6 +46,5 @@ public:
           }
         }
       }
-        
     }
 };
