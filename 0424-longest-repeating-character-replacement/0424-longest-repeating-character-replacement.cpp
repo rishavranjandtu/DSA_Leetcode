e20ad913efe3cd5be33ff50c08d7fac25d6ab5d1
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-04-01 14:25:12 +0530

    Time: 12 ms (63.52%), Space: 7.1 MB (16.80%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string a, int k) {
+        int i=0,j=0, maxRepeating = 0, maxLen = 0;
+        unordered_map<char, int> m;
+        
+        //Character other than maximum repeating character should at most be k, becuase we can make only k changes in the string.
+        //(length of substring - number of times of the maximum occurring character in the substring) <= k
+		
+        for(j=0; j<a.size(); j++){
+            m[a[j]]++;
+            maxRepeating = max(maxRepeating, m[a[j]]);
+            //When other characters become greater than k, we move window ahead.
+            if(j-i+1 - maxRepeating > k){
+                m[a[i]]--;
+                i++;
+            }
+            maxLen = max(maxLen, j-i+1);
+        }
+        return maxLen;
+    }
+};
