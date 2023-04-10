9a1b62afa9811f84e480ba754f8a8ba35964716a
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-04-10 21:51:53 +0530

    Time: 15 ms (69.53%), Space: 13.3 MB (78.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,27 +3,28 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
       int n=matrix.size();
       int m=matrix[0].size();
-      vector<int>r(n,1);
-      vector<int>c(m,1);
+      bool col0=false;
       for(int i=0;i<n;i++)
       {
-        for(int j=0;j<m;j++)
+       if(matrix[i][0]==0) col0=true; 
+        for(int j=1;j<m;j++)
         {
           if(matrix[i][j]==0)
           {
-            r[i]=0;
-            c[j]=0;
+           matrix[i][0]=0;
+            matrix[0][j]=0;
           }
         }
       }
       
-      for(int j=m-1;j>=0;j--)
-      {
-        for(int i=n-1;i>=0;i--)
-        {
-          if(r[i]==0||c[j]==0) matrix[i][j]=0;
-        }
-      }
+     for(int i=n-1;i>=0;i--)
+     {
+       for(int j=m-1;j>=1;j--)
+       {
+         if(matrix[i][0]==0||matrix[0][j]==0) matrix[i][j]=0;
+       }
+       if(col0) matrix[i][0]=0;
+     }
         
     }
 };
