a2ad7eccc735441c69a42a7ff5b3f87ce602a35a
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-05-27 12:44:38 +0530

    Time: 8 ms (56.33%), Space: 13.2 MB (40.43%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
+      int n=grid.size();
       int m=grid[0].size();
-      queue<pair<pair<int,int>,int>>q;
-       vector<vector<int>>vis(n,vector<int>(m,0));
+      queue<pair<int,pair<int,int>>>q;
+      vector<vector<int>>vis(n,vector<int>(m,0));
       for(int i=0;i<n;i++)
       {
         for(int j=0;j<m;j++)
         {
           if(grid[i][j]==2)
           {
-            q.push({{i,j},0});
-            vis[i][j]==1;
+            q.push({0,{i,j}});
+            vis[i][j]=1;
+            
           }
         }
       }
-      int dr[4]={0,0,-1,1};
-      int dc[4]={-1,1,0,0};
-      int t=0;
-     
+      
+      int ans=0;
+      int r[4]={-1,1,0,0};
+      int c[4]={0,0,1,-1};
       while(!q.empty())
       {
-        int x=q.front().first.first;
-        int y=q.front().first.second;
-         t=q.front().second;
+        auto f=q.front();
+        int d=f.first;
+        ans=d;
+        int x=f.second.first;
+        int y=f.second.second;
         q.pop();
-        for(int i=0;i<4;i++)
+        for(int j=0;j<4;j++)
         {
-          int nr=x+dr[i];
-          int nc=y+dc[i];
-          if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==1&&!vis[nr][nc])
+          int nr=x+r[j];
+          int nc=y+c[j];
+          if(nr>=0&&nr<n&&nc>=0&&nc<m&&!vis[nr][nc]&&grid[nr][nc]==1)
           {
             vis[nr][nc]=1;
-            q.push({{nr,nc},t+1});
+            q.push({d+1,{nr,nc}});
           }
         }
+        
       }
+      
       for(int i=0;i<n;i++)
       {
         for(int j=0;j<m;j++)
@@ -44,6 +49,7 @@ public:
           if(grid[i][j]==1&&!vis[i][j]) return -1;
         }
       }
-      return t;
+      return ans;
+        
     }
 };
