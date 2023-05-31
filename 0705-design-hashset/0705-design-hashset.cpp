2515b172def56cced6bca024f66a9508000623af
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-05-31 20:19:52 +0530

    Time: 225 ms (24.65%), Space: 40.9 MB (77.98%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,21 +1,45 @@
 class MyHashSet {
 public:
-    vector<int>v;
+  vector<list<int>>m;
     MyHashSet() {
-       v.resize(10000001,-1);
+      m.resize(100);
+        
     }
+  
+    int h_key(int n)
+    {
+      return n%11;
+    }
+  
+   list<int>::iterator search(int key)
+   {
+     
+     int i=h_key(key);
+     return find(m[i].begin(),m[i].end(),key);
+     
+   }
     
     void add(int key) {
-      v[key]=1;
+      if(contains(key)) return;
+      int i=h_key(key);
         
+      m[i].push_back(key);
     }
     
     void remove(int key) {
-        v[key]=-1;
+      if(contains(key)==false) return;
+      int i=h_key(key);
+      m[i].erase(search(key));
+        
     }
     
     bool contains(int key) {
-        return v[key]==1?true:false;
+      int i=h_key(key);
+      if(search(key)==m[i].end()) return false;
+      return true;
+      
+      
+        
     }
 };
 
