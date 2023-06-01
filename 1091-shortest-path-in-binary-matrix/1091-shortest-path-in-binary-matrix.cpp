105a7208af35f5a78751f8a0b56a13a2a3668ee8
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-06-01 10:06:38 +0530

    Time: 55 ms (98.01%), Space: 19.2 MB (85.79%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-         int n=grid.size();
-      int m=grid[0].size();
-      vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
-      dis[0][0]=0;
-      queue<pair<int,pair<int,int>>>q;
-      q.push({1,{0,0}});
-      if(grid[0][0]==1) return -1;
-      if(n==1&&m==1&&grid[0][0]==0) return 1;
+        int n=grid.size();
+        queue<pair<int,int>>q;
+      q.push({0,0});
+      if(grid[0][0]==1||grid[n-1][n-1]==1) return -1;
+      grid[0][0]=1;
       while(!q.empty())
       {
-        int d=q.front().first;
-        int r=q.front().second.first;
-        int c=q.front().second.second;
+        auto f=q.front();
+        int x=f.first;
+        int y=f.second;
+        if(x==n-1&&y==n-1) return grid[x][y];
         q.pop();
+        
         for(int i=-1;i<=1;i++)
         {
           for(int j=-1;j<=1;j++)
@@ -22,18 +21,16 @@ public:
             if(i==0&&j==0) continue;
             else
             {
-              int nr=r+i;
-              int nc=c+j;
-              if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==0&&1+dis[r][c]<dis[nr][nc])
+              int nr=x+i;
+              int nc=y+j;
+              if(nr>=0&&nr<n&&nc>=0&&nc<n&&grid[nr][nc]==0)
               {
-                if(nr==n-1&&nc==m-1) return d+1;
-                dis[nr][nc]=1+dis[r][c];
-                q.push({d+1,{nr,nc}});
+                grid[nr][nc]=1+grid[x][y];
+                q.push({nr,nc});
               }
             }
           }
         }
-    
       }
       return -1;
     }
