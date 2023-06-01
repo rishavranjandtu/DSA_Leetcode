8705312cbce2513de74224e60892bcc008072cbf
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-06-01 22:09:33 +0530

    Time: 4 ms (76.15%), Space: 9.1 MB (93.62%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>&s) {  
+    string longestCommonPrefix(vector<string>& s) {
       int n=s.size();
+      sort(s.begin(),s.end());
+      string a=s[0];
+      string b=s[n-1];
+      int m=min(a.size(),b.size());
       string ans="";
-      sort(s.begin(),s.end(),[](string a, string b){return a.size()>b.size();});
-      int m=s[0].size();
-     // for(auto x:s) m=min(m,x.size());
-      string y=s[0];
       for(int i=0;i<m;i++)
       {
-        char a=y[i];
-        for(int j=0;j<n;j++)
-        {
-          auto x=s[j];
-          if(x[i]!=a) return ans;
-        }
-        ans+=a;
+        if(a[i]==b[i]) ans+=a[i];
+        else break;
       }
-      return ans;
+        return ans;
     }
 };
