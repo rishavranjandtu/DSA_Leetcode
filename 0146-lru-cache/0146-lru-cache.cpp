c4f82390b1e1ab36bbc83ed262759acb7e645069
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-06-29 23:39:13 +0530

    Time: 592 ms (7.73%), Space: 172.9 MB (57.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,90 +1,85 @@
 class node
 {
   public:
-  int key;
   int data;
+  int key;
   node *next;
-  node *prev;
-  node(int k, int v)
+  node*prev;
+  node(int k,int d)
   {
     key=k;
-    data=v;
+    data=d;
     next=NULL;
     prev=NULL;
   }
+  
 };
+
+
 
 
 class LRUCache {
 public:
-    node*head;
-    node*tail;
-    unordered_map<int,node*>m;
-    int cap;
+   node *head;
+   node*tail;
+    int c;
+    map<int,node*>m;
     LRUCache(int capacity) {
-        cap=capacity;
-        head=new node(-1,-1);
-        tail=new node(-1,-1);
-        head->next=tail;
-        tail->prev=head;
+      c=capacity;
+      head=tail=new node(-1,-1);
+      head->next=tail;
+      tail->prev=head;
+      
+        
     }
-    
   
-     int del_n(node*b)
-     {
-       node*a=b->prev;
-       int v=b->data;
-       a->next=b->next;
-       b->next->prev=a;
-       return v;
-     }
+    void del(node*n)
+    {
+      if(n==tail)
+      {
+        node*a=n->prev->prev;
+        a->next=n;
+        n->prev=a;
+        
+      }
+      else
+      {
+        node*a=n->prev;
+        a->next=n->next;
+        n->next->prev=a;
+        
+      }
+    }
   
-    node* add_n(int k, int v)
+    node*add(int k, int v)
     {
       node*n=new node(k,v);
-      node*b=head->next;
+      node*tmp=head->next;
       head->next=n;
-      n->next=b;
-      b->prev=n;
+      n->next=tmp;
+      tmp->prev=n;
       n->prev=head;
+      
       return n;
     }
-  
-    int fun(node*n)
-    {
-      return n->key;
-    }
+    
     int get(int key) {
-      int v=-1;
-      if(m.find(key)!=m.end())
-      {
-        node*n=m[key];
-         v=del_n(n);
-        node *a=add_n(key,v);
-        m[key]=a;
-      }
-        return v;
+      
+      if(m.count(key)==0) return -1;
+      int f= m[key]->data;
+      del(m[key]);
+      node*n=add(key,f);
+      m[key]=n;
+      return f;
     }
     
     void put(int key, int value) {
-        if(m.find(key)!=m.end())
-        {
-          node*n=m[key];
-         int v=del_n(n);
-        node *a=add_n(key,value);
-        m[key]=a;
-        }
-      else{
-        if(m.size()>=cap)
-        {
-          int k=fun(tail->prev);
-          int a=del_n(tail->prev);
-          m.erase(k);
-        }
       
-          node*b=add_n(key,value);
-        m[key]=b;
-        }
+        if(m.count(key)!=0) del(m[key]);
+      else if(m.size()>=c) {m.erase(tail->prev->key); del(tail);}
+      node*n=add(key,value);
+      m[key]=n;
+        
     }
 };
 
