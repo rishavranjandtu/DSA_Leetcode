a5de5b34e26183f877b248fe19ab90dd885d8000
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-07-01 09:39:58 +0530

    Time: 113 ms (92.64%), Space: 59.7 MB (9.95%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -7,21 +7,9 @@ public:
       while(l<=h)
       {
         int m=l+(h-l)/2;
-        if(m>0&&m<n-1){
         if(arr[m]>arr[m-1]&&arr[m]>arr[m+1]) return m;
-        else if(arr[m]<arr[m+1]) l=m+1;
-        else h=m-1;}
-        
-        else if(m==0)
-        {
-          if(arr[m]>arr[m+1]) return m;
-          else return m+1;
-        }
-        else if(m==n-1)
-        {
-          if(arr[m]>arr[m-1]) return m;
-          else return m-1;
-        }
+        else if(arr[m]>arr[m+1]) h=m;
+        else l=m;
       }
       return l;
         
