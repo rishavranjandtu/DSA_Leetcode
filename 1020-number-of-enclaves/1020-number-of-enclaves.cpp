ebfc945cb2344087fd46125c1562e30c8f8cf344
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-07-09 15:18:44 +0530

    Time: 95 ms (22.95%), Space: 32.9 MB (36.05%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
-  int n;
-  int m;
-  int dr[4]={0,0,1,-1};
-  int dc[4]={-1,1,0,0};
-  void dfs(int a,int b, vector<vector<int>>&vis,vector<vector<int>>&grid)
-  {
-    vis[a][b]=1;
-    for(int i=0;i<4;i++)
+    int n;
+    int m;
+    int r[4]={0,0,-1,1};
+    int c[4]={-1,1,0,0};
+    void dfs(vector<vector<int>>&grid,int x,int y, vector<vector<int>>&vis)
     {
-      int x=a+dr[i];
-      int y=b+dc[i];
-      if(x>=0&&x<n&&y>=0&&y<m&&grid[x][y]==1&&!vis[x][y])
+      vis[x][y]=1;
+      for(int i=0;i<4;i++)
       {
-        dfs(x,y,vis,grid);
+        int nr=x+r[i];
+        int nc=y+c[i];
+        if(nr>=0&&nr<m&&nc>=0&&nc<n&&grid[nr][nc]==1&&!vis[nr][nc])
+        {
+          dfs(grid,nr,nc,vis);
+        }
       }
     }
-    
-  }
     int numEnclaves(vector<vector<int>>& grid) {
-      n=grid.size();
-      m=grid[0].size();
-      vector<vector<int>>vis(n,vector<int>(m,0));
-      for(int i=0;i<n;i++)
+      m=grid.size();
+      n=grid[0].size();
+      vector<vector<int>>vis(m,vector<int>(n,0));
+      for(int i=0;i<m;i++)
       {
-        for(int j=0;j<m;j++)
+        for(int j=0;j<n;j++)
         {
-          if(i==0||i==n-1||j==0||j==m-1)
+          if(i==0||i==m-1||j==0||j==n-1)
           {
-            if(grid[i][j]==1&&!vis[i][j])
+            if(grid[i][j]==1)
             {
-              dfs(i,j,vis,grid);
+              dfs(grid,i,j,vis);
             }
           }
         }
       }
-      int c=0;
-      for(int i=0;i<n;i++)
+      int ans=0;
+      for(int i=0;i<m;i++)
       {
-        for(int j=0;j<m;j++)
+        for(int j=0;j<n;j++)
         {
-          if(grid[i][j]==1&&!vis[i][j])
-          {
-             c++;
-          }
+          if(grid[i][j]==1&&!vis[i][j]) ans++;
         }
       }
-      
-        return c;
+        return ans;
     }
 };
