7640e03d5279508c8648993331952c7aa89ba8ea
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-07-12 17:58:04 +0530

    Time: 26 ms (51.68%), Space: 13.3 MB (85.49%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& p) {
-      vector<int>adj[n];
-      vector<int>ans;
-      for(auto x:p)
-      {
-        adj[x[1]].push_back(x[0]);
-      }
-      vector<int>ind(n,0);
-      for(int i=0;i<n;i++)
-      {
-        for(auto x:adj[i])
-        {
-          ind[x]++;
-        }
-      }
+    vector<int> findOrder(int nu, vector<vector<int>>& p) {
+        int n=p.size();
+      vector<vector<int>>adj(nu);
+      vector<int>in(nu,0);
       queue<int>q;
       for(int i=0;i<n;i++)
       {
-        if(ind[i]==0) q.push(i);
+        adj[p[i][1]].push_back(p[i][0]);
+        in[p[i][0]]++;
       }
-    // vector<int>ans;
+      for(int i=0;i<nu;i++)
+      {
+        if(in[i]==0) q.push(i);
+      }
+      vector<int>ans;
       while(!q.empty())
       {
         int f=q.front();
@@ -28,11 +22,12 @@ public:
         ans.push_back(f);
         for(auto x:adj[f])
         {
-          ind[x]--;
-          if(ind[x]==0) q.push(x);
+          in[x]--;
+          if(in[x]==0) q.push(x);
         }
       }
-     if(n==ans.size()) return ans;
-      else return {};
+      vector<int>d;
+      return ans.size()==nu?ans:d;
+    
     }
 };
