a633b6dca8c1b562bc2af9d6d120bcd171511395
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-07-12 17:54:27 +0530

    Time: 26 ms (51.40%), Space: 13.1 MB (88.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& pre) {
-      vector<vector<int>>adj(n);
-      for(auto x:pre)
-      {
-        adj[x[1]].push_back(x[0]);
-      }
-      vector<int>ind(n,0);
-      for(int i=0;i<n;i++)
-      {
-        for(auto x:adj[i])
-        {
-          ind[x]++;
-        }
-      }
-      
+    bool canFinish(int nu, vector<vector<int>>& p) {
+      int n=p.size();
+      vector<vector<int>>adj(nu);
+      vector<int>in(nu,0);
       queue<int>q;
       for(int i=0;i<n;i++)
       {
-        if(ind[i]==0) q.push(i);
+        adj[p[i][1]].push_back(p[i][0]);
+        in[p[i][0]]++;
       }
-      vector<int>ans;
+      for(int i=0;i<nu;i++)
+      {
+        if(in[i]==0) q.push(i);
+      }
+      int c=0;
       while(!q.empty())
       {
         int f=q.front();
         q.pop();
-        ans.push_back(f);
+        c++;
         for(auto x:adj[f])
         {
-          ind[x]--;
-          if(ind[x]==0) q.push(x);
+          in[x]--;
+          if(in[x]==0) q.push(x);
         }
-        
       }
-      if(ans.size()==n) return true;
-      return false;
-        
+      return c==nu;
     }
 };
