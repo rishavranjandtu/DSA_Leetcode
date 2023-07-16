1dcf7c8cf00f893be53e8fd10aa492a1ce08b374
Rishav Ranjan <78717685+rishavranjandtu@users.noreply.github.com>
2023-07-16 23:10:07 +0530

    Time: 102 ms (42.03%), Space: 32.6 MB (73.95%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<long,long>>adj[n];
-      for(int i=0;i<roads.size();i++)
+        vector<vector<pair<int,int>>>adj(n);
+        vector<long>time(n,LONG_MAX);
+        vector<long>ways(n,0);
+        time[0]=0;
+        ways[0]=1;
+        int m=pow(10,9)+7;
+        
+      for(auto x:roads)
       {
-        adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
-         adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
+        adj[x[0]].push_back({x[1],x[2]});
+        adj[x[1]].push_back({x[0],x[2]});
       }
-      priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>p;
-      p.push({0,0});
-      vector<long >ways(n,0);
-      vector<long>dis(n,LONG_MAX);
-      dis[0]=0;
-      ways[0]=1;
-      long m=1e9+7;
-      while(!p.empty())
-      {
-        long c=p.top().first;
-        long node=p.top().second;
-        p.pop();
-        for(auto x:adj[node])
+        priority_queue<pair<long,long>,vector<pair<long,long>>,greater<pair<long,long>>>p;
+        p.push({0,0});
+        while(!p.empty())
         {
-          long adjn=x.first;
-          long w=x.second;
-          if(c+w<dis[adjn])
+          long t=p.top().first;
+          long node=p.top().second;
+          p.pop();
+          
+          for(auto x:adj[node])
           {
-            ways[adjn]=ways[node];
-            dis[adjn]=c+w;
-            p.push({c+w,adjn});
+            long d=x.second;
+            long adjn=x.first;
+            if((t+d)<time[adjn])
+            {
+              time[adjn]=(t+d);
+              p.push({t+d,adjn});
+              ways[adjn]=ways[node];
+            }
+            else if(t+d==time[adjn])
+            {
+               
+              ways[adjn]+=ways[node];
+              ways[adjn]%=m;
+            }
           }
-          else if(c+w==dis[adjn])
-          {
-            ways[adjn]=ways[adjn]+(ways[node]);
-            ways[adjn]%=m;
-          }
-        }
+        
       }
-      if(ways[n-1]==INT_MAX) return 0;
       return ways[n-1];
     }
 };
